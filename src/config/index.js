a03678395399760b1f92b33ec85542fe11892073
trewq1009 <trewq1009@gmail.com>
2022-09-29 16:27:42 +0900

    fix: config내용 수정

--- src/config/index.js
+++ src/config/index.js
@@ -3,13 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-    APP_NAME : process.env.APP_NAME,
-    APP_PORT : process.env.APP_PORT,
-    APP_MODE : process.env.APP_MODE,
+    APP_NAME : process.env.APP_NAME || "Test First App",
+    APP_PORT : process.env.APP_PORT || 8000,
+    APP_MODE : process.env.APP_MODE || "dev",
 
-    DB_HOST : process.env.DB_HOST,
-    DB_USER : process.env.DB_USER,
-    DB_PASSWORD : process.env.DB_PASSWORD,
-    DB_NAME : process.env.DB_NAME,
-    DB_PORT : process.env.DB_PORT,
+    DB_HOST : process.env.DB_HOST || "localhost",
+    DB_USER : process.env.DB_USER || "admin",
+    DB_PASSWORD : process.env.DB_PASSWORD || "Rhksflwk1!",
+    DB_NAME : process.env.DB_NAME || "iderms_config",
+    DB_PORT : process.env.DB_PORT || 3306,
 }

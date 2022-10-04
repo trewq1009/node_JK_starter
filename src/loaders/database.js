b964b7e98a94ce25273d78e21ef93e423b21dac9
trewq1009 <trewq1009@gmail.com>
2022-10-04 13:47:56 +0900

    fix: connecting error message 수정

--- src/loaders/database.js
+++ src/loaders/database.js
@@ -1,18 +1,26 @@
 import db from "mysql";
 import config from "../config/index";
-import Logger from "./logger";
+import {Logger} from "./logger";
 
 export default async () => {
-    Logger.info('DB connecting...');
+    Logger.info('DB (Mysql) Create Connection');
     let connection = db.createConnection({
         host     : config.DB_HOST,
         user     : config.DB_USER,
         password : config.DB_PASSWORD,
         database : config.DB_NAME,
         port     : config.DB_PORT
-      });
-    connection.connect(err => {
-      if(err) Logger.error('DB connection Error : ' + err);
     });
+
+    try {
+        Logger.info('DB Connecting.....');
+        connection.connect(err => {
+            if(err) Logger.error('DB connection Error : ' + err);
+        });
+    } catch (e) {
+        Logger.info('DB Connecting Error!!!!');
+        Logger.debug(`DB Connecting Error Message: ${e.message}`);
+    }
+    
     return connection
 }

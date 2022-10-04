c1a0712cd14e01a80bab9b63ab5ad0fcef8c6a23
trewq1009 <trewq1009@gmail.com>
2022-10-04 17:39:43 +0900

    fix: 주석 수정

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -5,13 +5,13 @@ import {Logger} from "./logger";
 
 export default async (app) => {
     /*
-        cluster 는 node 초기에 CPU 코어당 여러가지 프로세스를 생성하기 위해 사용되었으나
-        이 방식은 코드 관리에 있어서 더 어렵게 만들게 되어 사용을 하지 않는다 (22.09.29)
+    cluster 는 node 초기에 CPU 코어당 여러가지 프로세스를 생성하기 위해 사용되었으나
+    이 방식은 코드 관리에 있어서 더 어렵게 만들게 되어 사용을 하지 않는다 (22.09.29)
+    Cluster connection
+    import clusterLoader from "./cluster";
+    await clusterLoader();
     */
-    // Cluster connection
-    // import clusterLoader from "./cluster";
-    // await clusterLoader();
-
+   
     // Database connection
     const dbConnection = await databaseLoader();
     Logger.info('DataBase Intialized');

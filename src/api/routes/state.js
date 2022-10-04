64827a498008eeae24c3300a33e8db991abd9023
trewq1009 <trewq1009@gmail.com>
2022-10-04 13:52:56 +0900

    fix: logger Message

--- src/api/routes/state.js
+++ src/api/routes/state.js
@@ -1,11 +1,15 @@
 import express from "express";
+import {Container} from "typedi";
 
 const router = express.Router();
 
 export default (app) => {
     app.use('/state', router);
+    
+    let logger = Container.get('logger');
 
     router.get('/serverState', (req, res, next) => {
+        logger.info(`(get):/state/serverState`);
         return res.status(200).json({
             status : 'success',
             message : 'Server On'
